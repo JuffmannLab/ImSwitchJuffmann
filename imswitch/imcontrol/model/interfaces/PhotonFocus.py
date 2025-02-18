d68472cc6bd7298ae9181428e4bd48640944d888
thomaslab55 <150344866+thomaslab55@users.noreply.github.com>
2025-02-18 15:57:11 +0100

    PhotonFocus interface

--- imswitch/imcontrol/model/interfaces/PhotonFocus.py
+++ imswitch/imcontrol/model/interfaces/PhotonFocus.py
@@ -24,9 +24,11 @@ class PhotonFocusBitflowCamera:
         self.pfcam_port = pfcam_port
 
         self.camera = PhotonFocus.PhotonFocusBitFlowCamera(self.bitlfow_idx, self.bitflow_camfile, self.pfcam_port)
-        device_info = self.camera.get_device_info()
-        self.camera.open()
         self.camera.set_exposure(self.exposure_time)
+        self.roi_limits = self.camera.get_roi_limits()
+
+    def open(self):
+        self.camera.open()
 
     def close(self):
         self.camera.close()
@@ -43,26 +45,49 @@ class PhotonFocusBitflowCamera:
     def set_roi(self, roi):
         hstart, hend, vstart, vend = roi[:4]
         self.camera.set_roi(hstart, hend, vstart, vend)
-
-    def get_current_roi(self):
-        return self.camera.get_roi()
-
-    def get_aquistion_param(self):
-        return self.camera.get_acquisition_parameters()
-    
-    def get_frame_timings(self):
-        return self.camera.get_frame_timings()
     
     def set_frame_period(self, frame_period):
         self.camera.set_frame_period(frame_period)
+
+    def start_acquistion(self):
+        self.camera.start_acquisition()
+
+    def stop_acquistion(self):
+        self.camera.stop_acquisition()
 
     def getLast(self):
         try:
             return self.camera.snap()
         except:
             pass
-         
+    
+    def get_attributes(self, attribute_name):
+        if attribute_name == 'ExposureTime':
+            attribute_value = self.camera.get_attribute_value('ExposureTime')
+        elif attribute_name == 'ROI':
+            attribute_value = self.camera.get_roi()
+        elif attribute_name == 'FramePeriod':
+            attribute_value = self.camera.get_frame_period()
+        elif attribute_name == 'BlackLevel':
+            attribute_value = self.camera.get_black_level_offset()
+        return attribute_value
 
+    def set_attributes(self, attribute_name, attribute_value):
+        if attribute_name == 'ExposureTime':
+            self.camera.set_attribute_value('ExposureTime', attribute_value)
+        elif attribute_name == 'ROI':
+            if attribute_value[0] <= self.roi_limits[0] and attribute_value[0] >= 0:
+                if attribute_value[1] <= self.roi_limits[1] and attribute_value[1] >= 0:
+                    self.camera.set_roi(attribute_value)
+                else: 
+                    self._logger.error('ROI Error')
+                    self._logger.info('ROI is out of bounds')
+            else: 
+                self._logger.error('ROI Error')
+                self._logger.info('ROI is out of bounds')
+
+
+        return attribute_value
 
 
 
