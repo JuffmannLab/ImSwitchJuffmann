031895bdbaa1001ef855b8ea417324b5f8e4af5d
thomaslab55 <150344866+thomaslab55@users.noreply.github.com>
2025-02-24 16:10:05 +0100

    PCO Interface Update

--- imswitch/imcontrol/model/interfaces/PCOCamera.py
+++ imswitch/imcontrol/model/interfaces/PCOCamera.py
@@ -12,8 +12,9 @@ from logging import raiseExceptions
 from imswitch.imcommon.model import initLogger
 
 class PCOCamera:
-    def __init__(self, idx = 0, cam_interface = None, reboot_on_fail = True, exposure_time = 0.001, ):
-        
+    def __init__(self, idx = 0, cam_interface = None, reboot_on_fail = True, exposure_time = 1, ):
+        self.__logger = initLogger(self, tryInheritParent=True)
+
         # camera parameters
         self.camera = None
         self.camera_idx = idx
@@ -28,6 +29,8 @@ class PCOCamera:
         
         self.camera = PCO.SC2.PCOSC2Camera(idx, cam_interface, reboot_on_fail)
 
+        self.pco_vid = []
+
     def open(self):
         self.camera.open()
 
@@ -40,25 +43,93 @@ class PCOCamera:
     def stopAcquistion(self):
         self.camera.stop_acquisition()
 
-    def getLatest(self):
-        self.camera.snap()
+    def getLast(self):
+        try:
+            return self.camera.snap()
+        except:
+            pass
 
-    def getChunk(self):
-        pass
+    def getLastChunk(self):
+        while True:    
+            try:
+                self.camera.wait_for_frame()
+                pco_newframe = self.camera.read_newest_image()
+                if type(pco_newframe) == np.ndarray:
+                    self.pco_vid.append(pco_newframe) 
+                pco_video = np.array(self.pco_vid)
+                return pco_video
+            except Exception as e:
+                self.__logger.error(e)
+                self.__logger.warning(f'Something went wrong in acquiring a video')
+
+    def setExposure(self, exposuretime):
+        self.exposure_time = exposuretime
+        self.setPropertyValue('exposure', self.exposure_time*0.001)
+
+
+    def reboot(self):
+        self.camera.reboot(wait=True)
 
     def getPropertyValue(self, property_name):
         if property_name == 'all':
-            self.camera.get_settings()
+            property_value = self.camera.get_settings()
         elif property_name == 'exposure':
-            self.camera.get_exposure()
-        elif property_name == ''
-        elif property_name ==
-        elif property_name ==
-        elif property_name ==
-        elif property_name ==
-        elif property_name ==
-        elif property_name ==
-        elif property_name ==
+            property_value = self.camera.get_exposure()
+        elif property_name == 'indexing':
+            property_value = self.camera.get_settings('image_indexing')
+        elif property_name == 'frameformat':
+            property_value = self.camera.get_settings('frame_format')
+        elif property_name == 'frameinfoformat':
+            property_value = self.camera.get_settings('frame_info_format')
+        elif property_name == 'frameinfoperiod':
+            property_value = self.camera.get_settings('frame_info_period')
+        elif property_name == 'doubleimagemode':
+            property_value = self.camera.get_settings('double_image_mode')
+        elif property_name == 'roi':
+            property_value = self.camera.get_settings('roi')
+        elif property_name == 'roilimits':
+            property_value = self.camera.get_roi_limits()
+        elif property_name == 'triggermode':
+            property_value = self.camera.get_settings('trigger_mode')
+        elif property_name == 'framedelay':
+            property_value = self.camera.get_settings('frame_delay')
+        elif property_name == 'frameperiod':
+            property_value = self.camera.get_settings('frame_period')
+        elif property_name == 'bitalignment':
+            property_value = self.camera.get_settings('bit_alignment')
+        elif property_name == 'hotpixel':
+            property_value = self.camera.get_settings('hotpixel_correction')
+        elif property_name == 'noisefilter':
+            property_value = self.camera.get_settings('noise_filter')
+        elif property_name == 'statusline':
+            property_value = self.camera.get_settings('status_line')
+        elif property_name == 'pixelrate':
+            property_value = self.camera.get_settings('pixel_rate')
+        elif property_name == 'temperature':
+            property_value = self.camera.get_temperature()
+        elif property_name == 'conversionfactor':
+            property_value = self.camera.get_conversion_factor()
+        elif property_name == 'detectorsize':
+            property_value = self.camera.get_detector_size()
+        elif property_name == 'framestatus':
+            property_value = self.camera.get_frames_status()
+        return property_value
 
     def setPropertyValue(self, property_name, property_value):
-        pass
+        if property_name == 'exposure':
+            self.camera.set_exposure(property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+        elif property_name == '':
+            self.camera.set_device_variable(property_name ,property_value)
+
