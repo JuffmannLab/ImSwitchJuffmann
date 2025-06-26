62103e738b396423c8524ffb858b060e55df6603
Ilia Zykov <ilya.zykov998@gmail.com>
2025-06-26 17:54:57 +0200

    PF ROI Shift

--- imswitch/imcontrol/model/interfaces/PhotonFocus.py
+++ imswitch/imcontrol/model/interfaces/PhotonFocus.py
@@ -45,18 +45,35 @@ class PhotonFocusBitflowCamera:
         self.camera.close()
 
     def setROI_shift(self, hstart, vstart):
-        
+
+        def round_to_step_with_bound(value, step=8, upper_bound=None):
+
+            rounded = round(value / step) * step
+            
+            if upper_bound is not None and rounded > upper_bound:
+                rounded = (upper_bound // step) * step
+            
+            return int(rounded) if isinstance(value, int) else rounded
+
         max_roi = self.getPropertyValue('MaxROI')
         h_max = max_roi[0].max
         v_max = max_roi[1].max
+        h_step = max_roi[0].pstep
+        v_step = max_roi[1].pstep
         h_current = self.getPropertyValue('image_width')
         v_current = self.getPropertyValue('image_height')
         h_limit = h_max - h_current
         v_limit = v_max - v_current
 
-        if hstart <= h_limit and vstart <= v_limit:
+        hstart = round_to_step_with_bound(hstart, step=h_step, upper_bound=h_limit)
+        vstart = round_to_step_with_bound(vstart, step=v_step, upper_bound=v_limit)
 
-            self.camera.fast_shift_roi(hstart, vstart)
+        if hstart <= h_limit and vstart <= v_limit:
+            
+            print(hstart)
+            print(vstart)
+            return hstart, vstart
+        
         else:
             self.__logger.warning(
                 f'ROI shift exceeds maximum boundaries. '
@@ -64,7 +81,6 @@ class PhotonFocusBitflowCamera:
                 f'Maximum vertical start: {v_limit}. '
                 f'Requested: {hstart},{vstart}')
         
-        return hstart, vstart
 
         
     def setROI(self, hstart, hend, vstart, vend):
@@ -72,6 +88,10 @@ class PhotonFocusBitflowCamera:
         # therefore it is rounding to the next instance of 128.
         # if the starting value + the roi size exceeds the frame size (1024)
         # the starting values will be changed accordingly 
+        
+        # The code is useless, because the pylablib class Bitflowcamera can not
+        # read the camfile, as it is binary but asks for xlm format. I did not find
+        # a way around this yet, other than rewriting pylablib.  
 
         def clamp_and_snap(value, min_val, max_val, step):
             value = max(min_val, min(max_val, value))
@@ -112,13 +132,17 @@ class PhotonFocusBitflowCamera:
     def suspend_live(self):
         if self.camera.acquisition_in_progress() == True:
             self.camera.pausing_acquisition()
-            
-    def getLastChunk(self):
-        try:
-            self.camera.wait_for_frame()
-            pf_newframe = self.camera.read_newest_image()
-            return pf_newframe
 
+    def flushBuffer(self):
+        self.camera.BufferManager(self.camera).reset()
+        self.camera.clear_acquisition()
+
+    def getLastChunk(self):
+
+        try: 
+            vid = self.camera.grab(nframes=self.nframes, frame_timeout=4) # unfortunately 100 frames is the maximum buffer size
+            return vid
+        
         except Exception as e:
             self.__logger.error(e)
             self.__logger.warning(f'Something went wrong in acquiring a video')
@@ -146,9 +170,9 @@ class PhotonFocusBitflowCamera:
         elif attribute_name == 'CameraName':
             attribute_value = self.camera.get_attribute_value('CameraName')
         elif attribute_name == 'Hstart':
-            attribute_value = self.camera.get_all_attribute_values('Window/X')
+            attribute_value = self.camera.get_attribute_value('Window/X')
         elif attribute_name == 'Vstart':
-            attribute_value = self.camera.get_all_attribute_values('Window/Y')
+            attribute_value = self.camera.get_attribute_value('Window/Y')
             
         return attribute_value
             
@@ -161,10 +185,20 @@ class PhotonFocusBitflowCamera:
             self.camera.set_attribute_value('FineGain', attribute_value)
         elif attribute_name == 'BlackLevelOffset':
             self.camera.set_attribute_value('Voltages/BlackLevelOffset', attribute_value)
+        elif attribute_name == 'CFR':
+            self.camera.enable_CFR(attribute_value)
+        elif attribute_name == 'StatusLine':
+            self.camera.enable_status_line(attribute_value)
+        elif attribute_name == 'NumberOfFrames':
+            self.nframes = attribute_value
         elif attribute_name == 'Hstart':
-            self.camera.set_attribute_value('Window/X', attribute_value)
+            vstart = self.getPropertyValue('Vstart')
+            hshift, vshift = self.setROI_shift(hstart=attribute_value, vstart=vstart)
+            self.camera.set_attribute_value('Window/X', hshift)
         elif attribute_name == 'Vstart':
-            self.camera.set_attribute_value('Window/Y', attribute_value)
+            hstart = self.getPropertyValue('Hstart')
+            hshift, vshift = self.setROI_shift(hstart=hstart, vstart=attribute_value)
+            self.camera.set_attribute_value('Window/Y', vshift)
             
     def openPropertiesGUI(self):
         pass
--- test3.py
+++ test3.py
@@ -1,25 +1,24 @@
-import numpy as np
-frames = []
+def round_to_step_with_bound(value, step=8, upper_bound=None):
+    """
+    Rounds a number to the nearest multiple of step (default 8),
+    while ensuring it doesn't exceed an optional upper bound.
+    
+    Args:
+        value (int/float): Input value to round
+        step (int): Step size to round to (default 8)
+        upper_bound (int/float): Maximum allowed value (optional)
+    
+    Returns:
+        int: Rounded value that is a multiple of step and ≤ upper_bound (if specified)
+    """
+    # Round to nearest step
+    rounded = round(value / step) * step
+    
+    # If we have an upper bound and exceeded it, round down instead
+    if upper_bound is not None and rounded > upper_bound:
+        rounded = (upper_bound // step) * step
+    
+    return int(rounded) if isinstance(value, int) else rounded
 
-a1 = np.array([[1, 1],
-               [1, 1]])
-
-a2 = np.array([[2, 2],
-               [2, 2]])
-
-rec_frames = 10
-n = 0
-currentframe = 0
-
-while currentframe < rec_frames:
-    if isinstance(a1, np.ndarray):
-        frames.append(a1)
-        vid = np.array(frames)
-        n = len(vid)
-        
-    if n > 0:
-        it = currentframe
-        currentframe += 1
-        print(currentframe)
-
-print(vid.shape)
+test = round_to_step_with_bound(50, upper_bound=42)
+print(test)
