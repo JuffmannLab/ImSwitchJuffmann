9d5172cc8c659b3ef75223b005e2134c6e285a62
unknown <zweigelt@A240-JUFF-LAB01.maxperutzlabs.ac.at>
2025-07-23 13:53:54 +0200

    MonacoControlTest.py added as standalone test script to communicate with Monaco laser. Connection succesful.

---
+++ MonacoControlTest.py
@@ -0,0 +1,20 @@
+import telnetlib as tn
+import time
+MONACO = "192.168.0.5"
+PORT = 23                   #default telnet port
+SHELL_PROMPT = b'Monaco>'
+CMD = "?ALL"
+if __name__ == "__main__":
+    connection = tn.Telnet(MONACO, PORT)
+    output = connection.read_until(SHELL_PROMPT).decode('ascii')
+    # print(output)
+
+    tosend = CMD.encode('ascii') + b'\r\n'
+    connection.write(tosend)
+    buffer = connection.read_until(SHELL_PROMPT, timeout=5).decode('ascii')
+    print(buffer)
+
+    connection.write("?TSTLS".encode('ascii')+b'\r\n')
+    IP = connection.read_until(SHELL_PROMPT, timeout=5).decode('ascii')
+    print(IP)
+    connection.close()
