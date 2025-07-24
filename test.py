17b9151f5a405fac6e8ec33c9a6b3117fad7d14e
thomaslab55 <150344866+thomaslab55@users.noreply.github.com>
2025-07-24 16:47:36 +0200

    implementing mcl microdrive

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-import napari
-import numpy as np
-import matplotlib.pyplot as plt
+from imswitch.imcontrol.model.interfaces.MCL_microdrive import MicroDrive
 
-def add_handler(detector, liveView=False):
-
-    test_main_handler = {}
-    handler = np.random.randint(2**31)
-
-    if detector not in test_main_handler:
-        test_main_handler[detector] = set()
-
-    test_main_handler[detector].add(handler)
-
-    return test_main_handler
-
-my_dict = {'Cam1': 564784558
-           }
-print(my_dict)
-def reading_dict(dict):
-    handles = [value if isinstance(value, set) else {value} for value in dict.values()]
-    values = [next(iter(value_set)) for value_set in handles]
-    for value in values:
-        print(value)
-
-reading_dict(my_dict)
+mcl = MicroDrive()
