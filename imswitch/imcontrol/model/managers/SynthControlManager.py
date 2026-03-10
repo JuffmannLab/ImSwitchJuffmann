c291c8f4a29644a9f9d03063fc13e1ee1b3d4b7a
Ilia Zykov <ilya.zykov998@gmail.com>
2026-03-10 13:47:02 +0100

    started on image downloading in manager, not working yet

--- imswitch/imcontrol/model/managers/SynthControlManager.py
+++ imswitch/imcontrol/model/managers/SynthControlManager.py
@@ -1,14 +1,34 @@
-import imslib as ims
+import imslib as imslib
+from pathlib import Path
+
 from imswitch.imcommon.model import initLogger
 
 class SynthControlManager:
     def __init__(self, setupInfo):
         self.__logger = initLogger(self)
         self._setupInfo = setupInfo
-        self.__logger.info(f"Targeted Scanning for IsoMet System with ID: {self._setupInfo.systemID}")
-        self.connection = ims.ConnectionList(max_discover_timeout_ms=100) #milliseconds
-        self.synth = self.targeted_system_scan(setupInfo.systemID)
+        self.__logger.info(f"Targeted Scanning for Isomet System with ID: {self._setupInfo.systemID}")
+        self.connection = imslib.ConnectionList(max_discover_timeout_ms=100) #milliseconds
+        synth = self.targeted_system_scan(setupInfo.systemID)
+        if synth is not None:
+            self.ims = synth
+            self.ims.Connect()
+            self.downloadImages()
 
+
+    def downloadImages(self):
+        imagePath = Path(__file__).resolve().parents[4]
+        imagePath = imagePath / "synthImages"
+        for imagefile in imagePath.glob("*.iip"):
+            name = imagefile.name
+            prj = imslib.ImageProject(str(imagefile))
+            group = prj.ImageGroupContainer[0]
+            for image in group:
+                dl = imslib.ImageDownload(self.ims, image)
+                dl.StartDownload()
+
+        print(imslib.ImageTableViewer(self.ims))
+        return
 
     def targeted_system_scan(self, systemID):
         synth = self.connection.Find("CM_USBLITE", systemID)
