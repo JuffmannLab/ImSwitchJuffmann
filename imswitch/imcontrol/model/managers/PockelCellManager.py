69094c0ffa03f9ce0eaca61a3b83cf552703c9ad
CisseUniVie <cisse.de.locht@univie.ac.at>
2026-03-23 12:18:59 +0100

    Implemented hardware functions of Pockelcellmanager. On program exit, the lines are all reset to 0V

--- imswitch/imcontrol/model/managers/PockelCellManager.py
+++ imswitch/imcontrol/model/managers/PockelCellManager.py
@@ -2,6 +2,8 @@ from imswitch.imcommon.framework import Signal, SignalInterface
 from imswitch.imcommon.model import initLogger
 import PyDAQmx as pd
 import numpy as np
+import time
+import atexit
 
 reset_line = "Dev1/port1/line1" #pin 4
 HV_line = "Dev1/port1/line2" #pin 5
@@ -29,22 +31,37 @@ class PockelCellManager(SignalInterface):
             self.digitalOutTask.StartTask()
         except Exception as e:
             self.__logger.error(f"Exception caught when connecting to NIDAQ device: {e}")
+        atexit.register(self.cleanup)
 
-    def __del__(self):
+    def cleanup(self):
         #Make sure that control lines and voltage are set to zero when imswitch quits.
-        data = np.array([False, False])
+        self.__logger.info("Clean up function called")
+        data = np.array([False, False], dtype=np.uint8)
         self.digitalOutTask.WriteDigitalLines(1, 1, 10.0, pd.DAQmx_Val_GroupByChannel, data, pd.byref(self.samplesWritten), None)
         self.analogOutTask.WriteAnalogScalarF64(1, 10.0, 0, None)
         self.analogOutTask.StopTask()
         self.digitalOutTask.StopTask()
 
     def sendVoltage(self, control_voltage):
-        pass
+        #make sure voltage stays within limits of pockelcell
+        voltage = max(0, min(control_voltage, 5))
+        self.analogOutTask.WriteAnalogScalarF64(1, 10.0, voltage, None)
 
-    def setHV(self, HV_ON):
-        pass
+    def setHV(self, HV):
+        self.hv = HV
+        data = np.array([self.reset, self.hv], dtype=np.uint8)
+        self.digitalOutTask.WriteDigitalLines(1, 1, 10.0, pd.DAQmx_Val_GroupByChannel, data, pd.byref(self.samplesWritten), None)
+
+
 
     def sendReset(self):
-        pass
+        data = np.array([True, self.hv], dtype=np.uint8)
+        self.digitalOutTask.WriteDigitalLines(1, 1, 10.0, pd.DAQmx_Val_GroupByChannel, data, pd.byref(self.samplesWritten), None)
+        self.__logger.info("Reset triggered! Sleeping for 2s for signal to propagate to PSU...")
+        time.sleep(2)
+        data[0] = False
+        self.digitalOutTask.WriteDigitalLines(1, 1, 10.0, pd.DAQmx_Val_GroupByChannel, data, pd.byref(self.samplesWritten), None)
+        self.__logger.info("Reset line put back to 0")
+
 
 
